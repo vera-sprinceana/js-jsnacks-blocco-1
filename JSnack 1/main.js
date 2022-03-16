b4055b58fbc8ee48bb8dc183610265ac7ce4d1f0
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-16 11:59:54 +0100

    JSnack 1

---
+++ JSnack 1/main.js
@@ -0,0 +1,7 @@
+let numero1=prompt("inserisci il 1° numero")*1;
+let numero2=prompt("inserisci il 2° numero")*1;
+if(numero1>=numero2){
+    console.log("il maggiore è" +numero1);
+}else{
+    console.log("il maggiore è" +numero2);
+}
